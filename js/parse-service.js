cf987bc2b05f6bf73f7825ba035e925bf1f39473
Lin Zhang <marshluca@gmail.com>
2013-10-31 06:03:17 +0800

    create a new page from blank template

--- js/parse-service.js
+++ js/parse-service.js
@@ -1,5 +1,7 @@
 Parse.initialize("zUNltvbSGI2Jh1j8hJwD9acWtdbxAA5XzmmS95w7", "Tp83XbAPCCFHnse06AwJ7hBekJXagRGNYVmuBJPz");
 
+var blankTemplateData = "[{\"id\":0,\"variation_name\":\"A\",\"page_blocks\":[{\"id\":\"page_block_header\",\"height\":\"83px\"},{\"id\":\"page_block_above_fold\",\"height\":\"586px\"},{\"id\":\"page_block_below_fold\",\"height\":\"422px\"},{\"id\":\"page_block_footer\",\"height\":\"55px\"}],\"elements\":[],\"background\":{\"color\":\"ffffff\",\"image\":null,\"position\":\"top left\",\"repeat\":\"repeat\",\"page\":{\"background_color\":\"transparent\"}},\"font\":\"Open Sans\",\"title\":\"Page title for search engines\",\"description\":\"Page description for search engines\",\"plug\":\"Free Landing Page\",\"name\":\"Demo4\",\"traffic\":[{\"date\":\"2013-10-23\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-24\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-25\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-26\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-27\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-28\",\"visits\":0,\"conversions\":0},{\"date\":\"2013-10-29\",\"visits\":0,\"conversions\":0}]}]"
+
 var ParseService = function () {
 
     this.getAllPages = function () {
@@ -41,6 +43,47 @@ var ParseService = function () {
         error: function(error) {
           alert("Error: " + error.code + " " + error.message);
           failureCallback(error);
+        }
+      });
+    };
+
+    this.createPage = function (pageName) {
+      // TODO: associate with user
+      var Page = Parse.Object.extend("Page");
+      var page = new Page();             
+      page.save({        
+        version: 1,              
+        title: pageName,        
+        shortTitle: pageName,
+        status: "stopped",
+        plug: "Free Landing Page"
+      }, {
+        success: function(page) {
+          // The object was saved successfully.          
+          console.log(page);
+          // create a PageItem with blank template
+          var PageItem = Parse.Object.extend("PageItem");
+          var pageItem = new PageItem();             
+          pageItem.save({
+            version: 1,
+            pageId: page.id,
+            data: blankTemplateData,
+            plug: "Free Landing Pages"
+          }, {
+            success: function(pageItem) {
+              // The object was saved successfully.
+              window.location = "/builder/editor.html?id=" + page.id;
+            },
+            error: function(pageItem, error) {
+              // The save failed.
+              // error is a Parse.Error with an error code and description.
+            }
+          });          
+        },
+        error: function(object, error) {
+          // The save failed.
+          // error is a Parse.Error with an error code and description.
+          alert('Failed to create new page, with error code: ' + error.description);          
         }
       });
     };
